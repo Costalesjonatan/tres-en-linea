524bce9c669849466213f2f7df940112aca5cc13
Costalesjonatan <costalesjonatan@gmail.com>
2025-06-01 11:40:43 -0300

    Se extrae extructura común a Row.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 export default function Board() {
   return (
     <>
-      <div className="board-row">
-        <Square number={1}/>
-        <Square number={2}/>
-        <Square number={3}/>
-      </div>
-      <div className="board-row">
-        <Square number={4}/>
-        <Square number={5}/>
-        <Square number={6}/>
-      </div>
-      <div className="board-row">
-        <Square number={7}/>
-        <Square number={8}/>
-        <Square number={9}/>
-      </div>
+      <Row valueOne={1} valueTwo={2} valueThree={3}/>
+      <Row valueOne={4} valueTwo={5} valueThree={6}/>
+      <Row valueOne={7} valueTwo={8} valueThree={9}/>
     </>
   );
 }
 
+function Row({ valueOne, valueTwo, valueThree }) {
+  return (
+      <div className="board-row">
+        <Square number={valueOne}/>
+        <Square number={valueTwo}/>
+        <Square number={valueThree}/>
+      </div>
+  );
+}
+
 function Square({ number }) {
-  return <button className="square">{ number }</button>
+  return ( <button className="square">{ number }</button> );
 }
